da4c73c174bcc4cdfa35d21444247698ceb2edcc
oguzhanakca <oguzhan.akca@hotmail.com>
2024-02-24 14:05:24 +0100

    get column datas

--- run.py
+++ run.py
@@ -25,6 +25,13 @@ def get_sales_data():
             break
     return sales_data
 
+def get_last_5_entries_sales():
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for i in range(1,7):
+        column = sales.col_values(i)
+        columns.append(column)
+
 
 def validate_data(values):
     try:
